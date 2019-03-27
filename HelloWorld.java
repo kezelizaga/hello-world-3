ff67d2095b83dae96dcf83249d1a6f4464763833
guelascarmella <43661692+guelascarmella@users.noreply.github.com>
2019-03-27 12:10:54 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+
+package helloworld;
+
+
+public class HelloWorld {
+
+  
+    public static void main(String[] args) {
+       int x = 5;
+       int y = 3;
+       
+       int total = x + y;
+        System.out.println("Sum: " + total);
+    }
+    
+}
